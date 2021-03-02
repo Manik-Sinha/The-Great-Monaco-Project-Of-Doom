87d451c24422eefb6eb8f1f5fcf0f44d28e7165e
Reavenk <Reavenk@gmail.com>
2021-03-02 00:14:56 -0600

    PNGExtraction spits the raw bit information along with lvl

--- Tools/MonacoFileTest/PNGExtraction/PNGExtraction.cpp
+++ Tools/MonacoFileTest/PNGExtraction/PNGExtraction.cpp
@@ -12,12 +12,16 @@ int main()
     std::vector<unsigned char> image; //the raw pixels
     unsigned width, height;
 
+
+    //std::string extractTarget = "C:\\Users\\Reavenk\\Desktop\\MonacoFiles\\InsertionTest.png";
+    std::string extractTarget = "C:\\Users\\Reavenk\\Desktop\\MonacoFiles\\Prison_Break.png";
+
     unsigned error =
         lodepng::decode(
             image,
             width,
             height,
-            "C:\\Program Files (x86)\\Steam\\userdata\\10170710\\113020\\remote\\Worlds\\Stage1.png"
+            extractTarget
             //"C:\\Users\\Reavenk\\Desktop\\MonacoFiles\\Prison_Break.png"
         );
 
@@ -39,6 +43,7 @@ int main()
 
     const bool RAW_DATA = false;
     std::vector<char> reconstr;
+    std::vector<char> rawExtracted;
 
     if (RAW_DATA == true)
     {
@@ -59,6 +64,42 @@ int main()
         // version with 3 bytes.
         for (size_t i = 0; i + 23 < bits.size(); i += 24)
         {
+
+            char ra = 0;
+            char rb = 0;
+            char rc = 0;
+            if (bits[i +  0] == true) { ra |= 1 << 0; }
+            if (bits[i +  1] == true) { ra |= 1 << 1; }
+            if (bits[i +  2] == true) { ra |= 1 << 2; }
+            if (bits[i +  3] == true) { ra |= 1 << 3; }
+            if (bits[i +  4] == true) { ra |= 1 << 4; }
+            if (bits[i +  5] == true) { ra |= 1 << 5; }
+            if (bits[i +  6] == true) { ra |= 1 << 6; }
+            if (bits[i +  7] == true) { ra |= 1 << 7; }
+
+            if (bits[i +  8] == true) { rb |= 1 << 0; }
+            if (bits[i +  9] == true) { rb |= 1 << 1; }
+            if (bits[i + 10] == true) { rb |= 1 << 2; }
+            if (bits[i + 11] == true) { rb |= 1 << 3; }
+            if (bits[i + 12] == true) { rb |= 1 << 4; }
+            if (bits[i + 13] == true) { rb |= 1 << 5; }
+            if (bits[i + 14] == true) { rb |= 1 << 6; }
+            if (bits[i + 15] == true) { rb |= 1 << 7; }
+
+            if (bits[i + 16] == true) { rc |= 1 << 0; }
+            if (bits[i + 17] == true) { rc |= 1 << 1; }
+            if (bits[i + 18] == true) { rc |= 1 << 2; }
+            if (bits[i + 19] == true) { rc |= 1 << 3; }
+            if (bits[i + 20] == true) { rc |= 1 << 4; }
+            if (bits[i + 21] == true) { rc |= 1 << 5; }
+            if (bits[i + 22] == true) { rc |= 1 << 6; }
+            if (bits[i + 23] == true) { rc |= 1 << 7; }
+
+            rawExtracted.push_back(ra);
+            rawExtracted.push_back(rb);
+            rawExtracted.push_back(rc);
+
+
             char a = 0;
             char b = 0;
 
@@ -80,6 +121,11 @@ int main()
             if (bits[i + 22] == true) { b |= 1 << 1;}
             if (bits[i + 23] == true) { b |= 1 << 0;}
 
+            std::cout << "Bit block " << i << std::endl;
+
+            std::cout << "From Data [0] : " << bits[i + 11] << bits[i + 10] << bits[i + 8] << bits[i + 7] << bits[i + 5] << bits[i + 4] << bits[i + 2] << bits[i + 1] << std::endl;
+            std::cout << "From Data [1] : " << bits[i + 23] << bits[i + 22] << bits[i + 20] << bits[i + 19] << bits[i + 17] << bits[i + 16] << bits[i + 14] << bits[i + 13] << std::endl;
+
             if(
                 bits[i + 0 ] || bits[i +  3] || bits[i +  6] || bits[i +  9] ||
                 bits[i + 12] || bits[i + 15] || bits[i + 18] || bits[i + 21])
@@ -89,9 +135,8 @@ int main()
                 // observed on valid files.
 
                 std::cout << "Suspicious bit placement at bit block " << i << std::endl;
-                std::cout << 
-                    bits[i + 0 ] << bits[i +  3] << bits[i +  6] << bits[i +  9] << 
-                    bits[i + 12] << bits[i + 15] << bits[i + 18] << bits[i + 21] << std::endl;
+
+                std::cout << "Filler : " << bits[i + 0 ] << bits[i +  3] << bits[i +  6] << bits[i +  9] << bits[i + 12] << bits[i + 15] << bits[i + 18] << bits[i + 21] << std::endl;
             }
 
             // Write out the 2 bytes we extracted from the 3 bytes
@@ -103,9 +148,13 @@ int main()
     std::cout << "Reconstructed!\n";
     std::cout << "Saving ... \n";
 
-    std::ofstream fout("C:\\Users\\Reavenk\\Desktop\\Out.dat", std::ios::out | std::ios::binary);
+    std::ofstream fout("C:\\Users\\Reavenk\\Desktop\\Out_InsertionTest.dat", std::ios::out | std::ios::binary);
     fout.write((char*)&reconstr[0], reconstr.size());
     fout.close();
-
     std::cout << "Saved!\n";
+
+    std::ofstream foutRE("C:\\Users\\Reavenk\\Desktop\\Out_InsertionTestRE.dat", std::ios::out | std::ios::binary);
+    foutRE.write((char*)&rawExtracted[0], rawExtracted.size());
+    foutRE.close();
+
 }
